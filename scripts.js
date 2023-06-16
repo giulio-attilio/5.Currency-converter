3b12790093a48a5da4e333c5b4be60046530547b
giulio-attilio <giulioap@hotmail.com>
2023-06-16 11:00:54 -0300

    Added new currencies in scripts.js

--- scripts.js
+++ scripts.js
@@ -10,6 +10,8 @@ function convertValues() {
     const inputCurrencyValue = document.querySelector("#input-value").value
     const dolarToday = 4.9
     const euroToday = 5.3
+    const poundToday = 6.2
+    const francToday = 5.41
 
     if (currencySelect.value == "dolar") {
         inputResult.innerHTML = new Intl.NumberFormat("en-US", {
@@ -24,6 +26,20 @@ function convertValues() {
             currency: "EUR"
         }).format(inputCurrencyValue / euroToday)
 
+    }
+
+    if (currencySelect.value == "libra") {
+        inputResult.innerHTML = new Intl.NumberFormat("en-UK", {
+            style: "currency",
+            currency: "GBP"
+        }).format(inputCurrencyValue / poundToday)
+    }
+
+    if (currencySelect.value == "franco") {
+        inputResult.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "CHF"
+        }).format(inputCurrencyValue / francToday)
     }
 
     inputResutToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
@@ -46,6 +62,16 @@ function changeCurrency() {
     if (currencySelect.value == "euro") {
         labelFlag.innerHTML = "Euro",
             currencyFlag.src = "./assets/euro-flag.png"
+    }
+
+    if (currencySelect.value == "libra") {
+        labelFlag.innerHTML = "Libra",
+            currencyFlag.src = "./assets/UK-flag.jpeg"
+    }
+
+    if (currencySelect.value == "franco") {
+        labelFlag.innerHTML = "Franco",
+            currencyFlag.src = "./assets/swiss-flag.jpeg"
     }
 
     convertValues()
