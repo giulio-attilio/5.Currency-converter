4cf397c1d629402dbfd2f2dcc0b00bdb91c4c7ce
giulio-attilio <giulioap@hotmail.com>
2023-06-15 21:02:12 -0300

    Updates in index.html, styles.css, scripts.js

--- scripts.js
+++ scripts.js
@@ -1,34 +1,53 @@
 
 const convertButton = document.querySelector("#convert-button")
+const currencySelect = document.querySelector("#currency-select")
+const inputResult = document.querySelector(".input-result")
+const inputResutToConvert = document.querySelector(".input-result-to-convert")
+
+convertButton.addEventListener("click", convertValues)
 
 function convertValues() {
-    const inputCurrencyValue = document.querySelector(".input-value").value
-
+    const inputCurrencyValue = document.querySelector("#input-value").value
     const dolarToday = 4.9
+    const euroToday = 5.3
 
-    const convertionResult = inputCurrencyValue / dolarToday
+    if (currencySelect.value == "dolar") {
+        inputResult.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+        }).format(inputCurrencyValue / dolarToday)
+    }
 
-    const inputResutToConvert = document.querySelector(".input-result-to-convert")
+    if (currencySelect.value == "euro") {
+        inputResult.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "EUR"
+        }).format(inputCurrencyValue / euroToday)
+
+    }
 
     inputResutToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
         style: "currency",
         currency: "BRL"
     }).format(inputCurrencyValue)
-
-    const inputResult = document.querySelector(".input-result")
-
-    inputResult.innerHTML = new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: "USD"
-    }).format(convertionResult)
-
-    const valueConverted = document.querySelector(".input-result")
-    
 }
 
-convertButton.addEventListener("click", convertValues)
+currencySelect.addEventListener("change", changeCurrency)
 
+function changeCurrency() {
+    const labelFlag = document.getElementById("label-flag")
+    const currencyFlag = document.getElementById("currency-flag")
 
+    if (currencySelect.value == "dolar") {
+        labelFlag.innerHTML = "Dólar",
+            currencyFlag.src = "./assets/usa-flag.png"
+    }
 
+    if (currencySelect.value == "euro") {
+        labelFlag.innerHTML = "Euro",
+            currencyFlag.src = "./assets/euro-flag.png"
+    }
 
+    convertValues()
+}
 
